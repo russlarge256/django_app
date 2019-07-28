8e2997b3590bf23d7cfcf9ee5bd2db6b18f35564
russlarge256 <russlarge@gmail.com>
2019-07-27 17:53:59 -0700

    Updated files

--- mysite/mysite/polling/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from polling.views import list_view, detail_view
-
-urlpatterns = [
-    path("", list_view, name="poll_index"),
-    path("poll/<int:poll_id>", detail_view, name="poll_detail"),
-]
-
