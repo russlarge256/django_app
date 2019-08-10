1d8d1f13b0cc82e17893a7030e8c660ddc4725c1
russlarge256 <russlarge@gmail.com>
2019-08-10 13:06:42 -0700

    Adding Inline to Blogging Admin

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+# create inline model admin to represent categories on the post admin view
+class PostInLine(admin.TabularInline):
+
+    model = Category.post.through
+
+
+# create a customized ModelAdmin class for the post and category models
+class PostAdmin(admin.ModelAdmin):
+
+    inlines = [PostInLine,
+               ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    inlines = [PostInLine,
+               ]
+    exclude = ('posts',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+
+
